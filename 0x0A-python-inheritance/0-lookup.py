043e3b9a5cbf63fc5056fe7e8a5e60c1d492be7e
Glad225 <okorochaglad170@gmail.com>
2023-04-13 05:49:31 -0700

    python programming

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+The lookup function container
+"""
+
+
+def lookup(object_to_inspect):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
