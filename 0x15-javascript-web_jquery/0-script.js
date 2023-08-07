b2039e29b44423f608f0bb06ecac6b35bd30571d
Glad225 <okorochaglad170@gmail.com>
2023-08-07 13:43:58 +0000

    javascript

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+1. const el = document.querySelector('header');
+2. el.style.color = '#FF0000';
+3.
+4.
+5. //OR
+6. //document.querySelector("header").style.color = "#FF0000";
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,1 @@
+Project on 0x15-javascript-web_jquery
